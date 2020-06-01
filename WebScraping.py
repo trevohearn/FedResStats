fd05b313d965b9228c996f63396121846fc82ca4
Trevor O'Hearn <trevohearn@gmail.com>
2020-06-01 13:33:11 -0400

    scraping from all urls 1st table

---
+++ WebScraping.py
@@ -0,0 +1,13 @@
+#Trevor O'Hearn
+#6/1/2020
+#WebScraping.def ():
+    doc = "The property."
+    def fget(self):
+        return self.
+    def fset(self, value):
+        self. = value
+    def fdel(self):
+        del self.
+    return locals() = property(*())
+
+import requests
