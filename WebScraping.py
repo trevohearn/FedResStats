9524a883cb7c711d7a63426bcce271786af2c0be
Trevor O'Hearn <trevohearn@gmail.com>
2020-06-01 16:28:11 -0400

    scrape

--- WebScraping.py
+++ WebScraping.py
@@ -1,13 +1,13 @@
 #Trevor O'Hearn
 #6/1/2020
-#WebScraping.def ():
-    doc = "The property."
-    def fget(self):
-        return self.
-    def fset(self, value):
-        self. = value
-    def fdel(self):
-        del self.
-    return locals() = property(*())
+#WebScraping.py
+#Scrapes values from fed reserve weekly summary statement
 
 import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.federalreserve.gov/releases/h41/current/h41.htm'
+get = requests.get(url)
+bs = BeautifulSoup(get.content, 'html-parser')
+
+data = bs.select('.H41Release > tr > td > p')
