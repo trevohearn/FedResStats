e3524bd388bbf93c5cd3bb1c6e776ff799ecbac8
Trevor O'Hearn <trevohearn@gmail.com>
2020-06-02 18:11:55 -0400

    editing methods

--- WebScraping.py
+++ WebScraping.py
@@ -5,9 +5,57 @@
 
 import requests
 from bs4 import BeautifulSoup
+import WebScrapingMethods as wsm
+import pandas as pd
 
-url = 'https://www.federalreserve.gov/releases/h41/current/h41.htm'
+
+base_url = 'https://www.federalreserve.gov/releases/h41/'
 get = requests.get(url)
 bs = BeautifulSoup(get.content, 'html-parser')
 
 data = bs.select('.H41Release > tr > td > p')
+
+dates = {'01' : ['02', '09','16','23','30'],
+        '02' : ['06', '13', '20' , '27'],
+        '03' : ['05', '12', '19' , '26'],
+        '04' : ['02', '09', '16', '23', '30'],
+         '05' : ['07', '14', '21'] #has current
+        }
+
+urls = wsm.getLinks(base_url, dates_dict=dates)
+
+dfs = []
+for url in urls:
+    bs = wsm.getSoup(url)
+        data = bs.select('.H41Release > tr > td > p')
+    if (len(data) == 0):
+        data = bs.select('.H41Release td p')
+        if (len(data) == 0):
+            print('no data')
+            print(url)
+            break;
+    features = wsm.getFeatures(bs)
+    clean_features = wsm.cleanFeatures(features)
+    clean_data = wsm.cleanData(data, len(clean_features[2]))
+    dfs.append(wsm.createDataFrame(clean_features, clean_data))
+df = dfs[0]
+for d in dfs[1:]:
+    df = df.append(d, ignore_index=False)
+df['Date'] = pd.to_datetime(df['Date'], format='%B %d, %Y', errors='ignore')
+df.set_index('Date', inplace=True)
+
+all_dfs = [df]
+columnNames = df.columns
+df.fillna('0', inplace=True)
+for df in all_dfs:
+    for c in columnNames:
+        df[c] = df[c].apply(removeUnicode)
+        df[c] = df[c].apply(removePlus)
+        df[c] = df[c].apply(removeComma)
+        df[c] = df[c].astype(int)
+#pd.to_datetime('13000101', format='%Y%m%d', errors='coerce')
+#rename df
+namesdict = {}
+for i, c in enumerate(df.columns):
+    namesdict[c] = c.strip('1234567890').title()
+df.rename(namesdict, axis='columns', inplace=True)
---
+++ WebScrapingMethods.py
@@ -0,0 +1,117 @@
+# Trevor O'Hearn
+# 6/2/20
+# Methods to help Scrape Fed Reserve
+
+#imports
+from bs4 import BeautifulSoup
+import pandas as pd
+
+
+#given dictionary, or month(s), day(s), year(s)
+def getLinks(base_url, dates_dict=None, months=None, days=None, years=None):
+    urls = []
+    if (type(dates_dict) == dict):
+        for d in dates_dict.keys():
+            for v in dates[d]:
+                url = base_url + '2020{}{}/h41.htm'.format(d, v)
+                urls.append(base_url)
+    elif (months) and (days) and (years):
+        for y in years:
+            for m in months:
+                for d in days:
+                    url = base_url + '{}{}{}/h41.htm'.format(y, m, d)
+                    urls.append(base_url)
+    elif (months) and (days):
+            for m in months:
+                for d in days:
+                    url = base_url + '{}{}{}/h41.htm'.format(y, m, d)
+                    urls.append(base_url)
+    urls.append(base_url + 'current/h41.htm')
+    return urls
+
+#returns beautifulsoup of html page
+def getSoup(url):
+    get = requests.get(url)
+    return BeautifulSoup(get.content, 'html.parser')
+
+#returns the feature names of the table
+def getFeatures(bs):
+    #identify what we want from each table
+    dids = {}
+    tables_bs = bs.select('table')
+    maxtable = len(tables_bs)
+    #starts at table 1 but tables_bs is 0 indexed
+    curtable = int(tables_bs[0].find('th').attrs['id'].strip('t').split('r')[0])
+
+    features = {}
+
+    while curtable <= maxtable:
+        #last table row
+        #all data elements in row
+        columns = len(tables_bs[curtable - 1].findAll('tr')[-1].findAll('td'))
+        rows = len(tables_bs[curtable -1].findAll('tr'))
+        dids[curtable] = []
+        #get all features for table
+        features[curtable] = tables_bs[curtable - 1].findAll('th', {'headers' : 't{}c0'.format(curtable)})
+        rows = len(features[curtable])
+        #get all data for table
+    #     for column in range(1, columns):
+    #         #append column of data to list for that table
+    #         dids[curtable].append(bs.select('#t{}c{}'.format(curtable, column)))
+
+        curtable += 1
+    return features
+
+#strips the data and returns the chosen features
+def cleanData(data, numFeatures, multiple=4, column=3, skip_amount=1, date_index=0):
+    #skip date
+    dataM = data[skip_amount:]
+    dvals = {}
+    dfvals = {'Date' : data[date_index].text.strip()}
+    cleanData = [dfvals['Date']]
+    for index in range(numFeatures):
+        cleanData.append(dataM[multiple * index + column].text.strip())
+    return cleanData
+
+#clean features scraped from the web
+#takes soup elements and returns dictionary of table and list of features
+#skips empty soup elements
+def cleanFeatures(features):
+    cleaned_features = {}
+    for key in features:
+        cleaned_features[key] = []
+        for value in features[key]:
+            if (len(value.text) != 0): #not empty row
+                cleaned_features[key].append(value.text.strip('\n !@#$%^&*()_'))
+            else: #do nothing
+                continue
+    return cleaned_features
+
+
+#returns the pandas dataframe from the given features and data
+def createDataFrame(cleaned_features, data, date_index=0):
+    #create df from cleaned_features
+    if (len(cleaned_features) != len(data)):
+        return 'None'
+    dvals = {}
+    dfvals = {'Date' : data.pop(date_index).text.strip()}
+    for key in data:
+        dfvals[key] = data
+    return pd.DataFrame(data=dfvals)
+
+#remove unic issues '\xa0'
+def removeUnicode(x):
+    if '\xa0' in str(x):
+        return x.replace('\xa0', '')
+    else:
+        return x
+def removePlus(x):
+    if '+' in str(x):
+        return x.replace('+', '')
+    else:
+        return x
+def removeComma(x):
+    if ',' in str(x):
+        return x.replace(',', '')
+    else:
+        return x
